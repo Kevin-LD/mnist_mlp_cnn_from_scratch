9179fe0ca620170c1d00bbfd0c1dc56b359d813f
Kevin Luo <ldkevin23307130031@gmail.com>
2026-05-18 16:59:03 +0800

    适配 CNN

--- test_train.py
+++ test_train.py
@@ -15,19 +15,19 @@ train_images_path = r'.\dataset\MNIST\train-images-idx3-ubyte.gz'
 train_labels_path = r'.\dataset\MNIST\train-labels-idx1-ubyte.gz'
 
 with gzip.open(train_images_path, 'rb') as f:
-        magic, num, rows, cols = unpack('>4I', f.read(16))
-        train_imgs=np.frombuffer(f.read(), dtype=np.uint8).reshape(num, 28*28)
+    magic, num, rows, cols = unpack('>4I', f.read(16))
+    train_imgs = np.frombuffer(f.read(), dtype=np.uint8).reshape(num, 28*28)
     
 with gzip.open(train_labels_path, 'rb') as f:
-        magic, num = unpack('>2I', f.read(8))
-        train_labs = np.frombuffer(f.read(), dtype=np.uint8)
+    magic, num = unpack('>2I', f.read(8))
+    train_labs = np.frombuffer(f.read(), dtype=np.uint8)
 
 
 # choose 10000 samples from train set as validation set.
 idx = np.random.permutation(np.arange(num))
 # save the index.
 with open('idx.pickle', 'wb') as f:
-        pickle.dump(idx, f)
+    pickle.dump(idx, f)
 train_imgs = train_imgs[idx]
 train_labs = train_labs[idx]
 valid_imgs = train_imgs[:10000]
@@ -39,13 +39,29 @@ train_labs = train_labs[10000:]
 train_imgs = train_imgs / train_imgs.max()
 valid_imgs = valid_imgs / valid_imgs.max()
 
-linear_model = nn.models.Model_MLP([train_imgs.shape[-1], 600, 10], 'ReLU', [1e-4, 1e-4])
-optimizer = nn.optimizer.SGD(init_lr=0.06, model=linear_model)
+
+# CNN 模型预处理；MLP 请注释下面两行
+train_imgs = train_imgs.reshape(-1, 1, 28, 28)  # 变为 [Batch, 1, 28, 28]
+valid_imgs = valid_imgs.reshape(-1, 1, 28, 28)  # 变为 [Batch, 1, 28, 28]
+
+# CNN 训练
+cnn_model = nn.models.Model_CNN()
+optimizer = nn.optimizer.SGD(init_lr=0.06, model=cnn_model)
 scheduler = nn.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[800, 2400, 4000], gamma=0.5)
-loss_fn = nn.op.MultiCrossEntropyLoss(model=linear_model, max_classes=train_labs.max()+1)
+loss_fn = nn.op.MultiCrossEntropyLoss(model=cnn_model, max_classes=train_labs.max()+1)
 
-runner = nn.runner.RunnerM(linear_model, optimizer, nn.metric.accuracy, loss_fn, scheduler=scheduler)
+runner = nn.runner.RunnerM(cnn_model, optimizer, nn.metric.accuracy, loss_fn, scheduler=scheduler)
 
+
+# MLP 训练
+# linear_model = nn.models.Model_MLP([train_imgs.shape[-1], 600, 10], 'ReLU', [1e-4, 1e-4])
+# optimizer = nn.optimizer.SGD(init_lr=0.06, model=linear_model)
+# scheduler = nn.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[800, 2400, 4000], gamma=0.5)
+# loss_fn = nn.op.MultiCrossEntropyLoss(model=linear_model, max_classes=train_labs.max()+1)
+# runner = nn.runner.RunnerM(linear_model, optimizer, nn.metric.accuracy, loss_fn, scheduler=scheduler)
+
+
+# 开始训练
 runner.train([train_imgs, train_labs], [valid_imgs, valid_labs], num_epochs=5, log_iters=100, save_dir=r'./best_models')
 
 _, axes = plt.subplots(1, 2)
